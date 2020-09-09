8653702cc96cd2a0269b2085e520ebbd4d439a43
David Clare <davidclare@Davids-MacBook-Pro.local>
2020-09-09 15:45:36 +0100

    more linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,13 +7,8 @@ module.exports = {
       jsx: true // Allows for the parsing of JSX
     }
   },
-  settings: {
-    react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
-    }
-  },
+  settings: {},
   extends: [
-    // "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
@@ -22,5 +17,7 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     '@typescript-eslint/no-var-requires': 0,
+    "indent": [2, 2],
   },
+  ignorePatterns: [ "dist/*", ".*" ] 
 };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import app from './app';
 
-console.log('hello world.......!!!!!!!dfdfdfd!!!');
-
 const server = app.listen(app.get('port'), () => {
     console.group();
     console.log('App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
