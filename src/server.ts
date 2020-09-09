57c5339c40519d06b5f488a7b3b50258d3e8b150
David Clare <davidclare@Davids-MacBook-Pro.local>
2020-09-09 17:36:12 +0100

    catch all route

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import app from './app';
 import { helloRouter } from "./services/hello/hello.router";
+import { Request, Response, NextFunction } from "express";
 
 // Routers
 app.use("/hi", helloRouter);
+
+app.use("*", (req: Request, res: Response, next: NextFunction) => {
+    res.statusMessage = "Not found";
+    res.status(404).json({ url: req.originalUrl });
+});
 
 const server = app.listen(app.get('port'), () => {
     console.clear()
