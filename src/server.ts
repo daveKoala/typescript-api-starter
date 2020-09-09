9aa262df29ef242a4ca8c03a53b69f9343428a75
David Clare <davidclare@Davids-MacBook-Pro.local>
2020-09-09 14:58:27 +0100

    add ts

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ console.log("hello world");
 
 const server = app.listen(app.get("port"), () => {
   console.group();
-  "  App is running at http://localhost:%d in %s mode",
+  console.log(
+    "  App is running at http://localhost:%d in %s mode",
         app.get("port"),
         app.get("env")
-
+  );
   console.groupEnd()
 })
 
