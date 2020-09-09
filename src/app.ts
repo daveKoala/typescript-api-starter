b30a4b7f72abd998223fea0bcfb7e85ff0032f0d
David Clare <davidclare@Davids-MacBook-Pro.local>
2020-09-09 15:34:05 +0100

    linter added

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
-import helmet from "helmet";
-import cors from "cors";
+import express from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
-app.use(bodyParser.json())
-.use(helmet())
-  .use(cors())
+app.use(bodyParser.json()).use(helmet()).use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
-import app from "./app";
+import app from './app';
 
-console.log("hello world.......");
+console.log('hello world.......!!!!!!!dfdfdfd!!!');
 
-const server = app.listen(app.get("port"), () => {
-  console.group();
-  console.log(
-    "  App is running at http://localhost:%d in %s mode",
-        app.get("port"),
-        app.get("env")
-  );
-  console.groupEnd()
-})
+const server = app.listen(app.get('port'), () => {
+    console.group();
+    console.log('App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
+    console.groupEnd();
+});
 
 export default server;
