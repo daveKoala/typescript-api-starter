ef36a5bba2109510137cf76b69db2c6a3e3ac17a
David Clare <davidclare@Davids-MacBook-Pro.local>
2020-09-09 17:50:57 +0100

    tweak message

--- src/services/hello/hello.router.ts
+++ src/services/hello/hello.router.ts
@@ -1,15 +1,27 @@
 import express, { Request, Response, NextFunction } from 'express';
 import versionRouter from '../../lib/versionRouter';
+
 export const helloRouter = express.Router();
 
 const routeMap = new Map();
 
 routeMap.set('apple', (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json({ message: 'hello to you version apple' });
+  return res.status(200).json({ message: 'GET: version apple' });
 });
 
 routeMap.set('orange', (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json({ message: 'hello to you version orange' });
+  return res.status(200).json({ message: 'GET: version orange' });
 });
 
 helloRouter.get('/', versionRouter(routeMap));
+
+const routeMap2 = new Map();
+
+routeMap2.set('apple', (req: Request, res: Response, next: NextFunction) => {
+  return res.status(200).json({ message: 'POST: version apple' });
+});
+
+routeMap2.set('orange', (req: Request, res: Response, next: NextFunction) => {
+  return res.status(200).json({ message: 'POST: version orange' });
+});
+helloRouter.post('/bye', versionRouter(routeMap2));
