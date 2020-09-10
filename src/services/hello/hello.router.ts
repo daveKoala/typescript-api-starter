6d894d761242a9ec3f2d291e5596517029626aa8
David Clare <davidclare@Davids-MacBook-Pro.local>
2020-09-10 11:50:39 +0100

    c

--- src/services/hello/hello.router.ts
+++ src/services/hello/hello.router.ts
@@ -4,25 +4,25 @@ import middleware from '../../middleware';
 
 export const helloRouter = express.Router();
 
-const routeMap = new Map();
+const hiMap = new Map();
 
-routeMap.set('apple', (req: Request, res: Response, next: NextFunction) => {
+hiMap.set('apple', (req: Request, res: Response, next: NextFunction) => {
   return res.status(200).json({ message: 'GET: version apple' });
 });
 
-routeMap.set('orange', (req: Request, res: Response, next: NextFunction) => {
+hiMap.set('orange', (req: Request, res: Response, next: NextFunction) => {
   return res.status(200).json({ message: 'GET: version orange' });
 });
 
-helloRouter.get('/', versionRouter(routeMap));
+helloRouter.get('/', versionRouter(hiMap));
 
-const routeMap2 = new Map();
+const byeMap = new Map();
 
-routeMap2.set('apple', (req: Request, res: Response, next: NextFunction) => {
+byeMap.set('apple', (req: Request, res: Response, next: NextFunction) => {
   return res.status(200).json({ message: 'POST: version apple' });
 });
 
-routeMap2.set('orange', (req: Request, res: Response, next: NextFunction) => {
+byeMap.set('orange', (req: Request, res: Response, next: NextFunction) => {
   return res.status(200).json({ message: 'POST: version orange' });
 });
-helloRouter.post('/bye', middleware.authentication, middleware.authorization('a role'), versionRouter(routeMap2));
+helloRouter.post('/bye', middleware.authentication, middleware.authorization('a role'), versionRouter(byeMap));
