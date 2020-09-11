c48c54c8a6a91b274b06d57e4411853c2fd9b607
David Clare <davidclare@Davids-MacBook-Pro.local>
2020-09-11 10:09:15 +0100

    lower case version

--- src/middleware/setAPIVersion.ts
+++ src/middleware/setAPIVersion.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export const setAPIVersion = (req: Request, res: Response, next: NextFunction) => {
   if (req && req.headers) {
     if (req.header('accept-version')) {
-      req.version = req.header('accept-version');
+      req.version = req.header('accept-version')?.toLowerCase();
     }
   }
   next();
